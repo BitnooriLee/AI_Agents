df896ee24b101a8353cc0c5f2f1b33423102430b
Bitnoori Lee <bitnoori.lee@gmail.com>
2026-01-22 08:46:17 +0100

    initial content-pipeline

---
+++ CONTENT-PIPELNIE-AGENT/tools.py
@@ -0,0 +1,46 @@
+import os, re
+from crewai.tools import tool
+from firecrawl import FirecrawlApp, ScrapeOptions
+
+@tool
+def web_search_tool(query:str):
+    """
+    Web Search Tool.
+    Args:
+        query: str
+            The query to search the web for.
+    Returns:
+        A list of search results with the website content in markdown format.
+    """
+    app = FirecrawlApp(api_key=os.getenv('FIRECRAWL_API_KEY'))
+
+    response = app.search(
+        query=query,
+        limit=3,
+        scrape_options=ScrapeOptions(
+            formats=["markdown"],
+        ),
+    )
+
+    if not response.success:
+        return "Error using tool"
+
+    cleaned_chunks = []
+
+    for result in response.data:
+        title = result["title"]
+        url = result["url"]
+        markdown = result["markdown"]
+
+        cleaned = re.sub(r'\\+|\n+', "", markdown).strip()
+        cleaned = re.sub(r"\[[^\]]+\]\([^\)]+\)|https?://[^\s]+", "", cleaned)
+        cleaned_result = {
+            "title": title,
+            "url": url,
+            "markdown": cleaned,
+        }
+        cleaned_chunks.append(cleaned_result)
+     
+    return cleaned_chunks
+
+#print(web_search_tool("AI Engineer jobs in Sweden remote"))
