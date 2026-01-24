9ea64d4b567b5b94479b2481ceb77fe87e244c6c
Bitnoori Lee <bitnoori.lee@gmail.com>
2026-01-24 21:43:32 +0100

    add refinement loop

--- CONTENT-PIPELNIE-AGENT/main.py
+++ CONTENT-PIPELNIE-AGENT/main.py
@@ -7,8 +7,13 @@ class ContentPipelineState(BaseModel):
     content_type: str = ""
     topic: str = ""
     #Internal 
-    max_characters: int = 0 
-
+    max_length: int = 0 
+    score: int = 0
+    #Content 
+    blog_post: str = ""
+    tweet: str = ""
+    linkedin_post: str = ""
+ 
 class ContentPipelineFlow(Flow[ContentPipelineState]):
     @start()
     def init_content_pipeline(self):
@@ -16,20 +21,23 @@ class ContentPipelineFlow(Flow[ContentPipelineState]):
             raise ValueError("The content type is wrong")
         if self.topic == "":
             raise ValueError("The topic can't be blank.")
+
         if self.state.content_type == "tweet":
-            self.state.max_characters = 150 
+            self.state.max_length = 150 
         elif self.state.content_type == "blog":
-            self.state.max_characters = 800
+            self.state.max_length = 800
         elif self.state.content_type == "linkedin":
             self.state.max_length = 500
-    @listen()
+
+    @listen(init_content_pipeline)
     def conduct_research(self):
         print("Researching....")
         return True
 
     @router(conduct_research)
-    def router(self):
+    def conduct_research_router(self):
         content_type = self.state.content_type
+        
         if content_type == "tweet":
             return "make_tweet"
         elif content_type == "blog":
@@ -39,31 +47,61 @@ class ContentPipelineFlow(Flow[ContentPipelineState]):
         else:
             raise ValueError("The content type is wrong")
     
-    @listen("make_blog")
+    @listen(or_("make_blog", "remake_blog"))
     def handle_make_blog(self):
+        #if blog post has been made, show the old one to the ai and ask it to improve, else 
+        #just ask to create. 
         print("Making blog post...")
-    @listen("make_tweet")
+
+    @listen(or_("make_tweet", "remake_tweet"))
     def handle_make_tweet(self):
+        #if tweet has been made, show the old one to the ai and ask it to improve, else 
+        #just ask to create. 
         print("Making tweet...")
-    @listen("make_linkedin_post")
+
+    @listen(or_("make_linkedin_post", "remake_linkedin_post"))
     def handle_make_linkedin_post(self):
+        # if post has been made, show the old one to the ai and ask it to improve, else
+        # just ask to create.
         print("Making linkedin post...")
+
     @listen(handle_make_blog)
     def check_seo(self):
         print("Checking Blog SEO")
+
     @listen(or_(handle_make_tweet, handle_make_linkedin_post))
     def check_virality(self):
-        print("Checking virality..")
-    @listen(or_(check_virality, check_seo))
+        print("Checking virality...")
+
+    @router(or_(check_seo, check_virality))
+    def score_router(self):
+
+        content_type = self.state.content_type
+        score = self.state.score
+
+        if score >= 8:
+            return "check_passed"
+        else:
+            if content_type == "blog":
+                return "remake_blog"
+            elif content_type == "linkedin":
+                return "remake_linkedin"
+            else:
+                return "remake_tweet"
+
+    @listen("check_passed")
     def finalize_content(self):
         print("Finalizing content...")
     
 
 
 flow = ContentPipelineFlow()
+'''
 flow.kickoff(
     inputs={
         "content_type": "tweet", 
         "topic": "AI Dog Training",
         },
 )
+'''
+flow.plot()
