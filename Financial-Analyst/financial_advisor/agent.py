dc902529fad83200d1d3aa4a2c53e02d7e887db9
Bitnoori Lee <bitnoori.lee@gmail.com>
2026-02-05 20:39:06 +0100

    add Tools and subagents, updates dependencies

--- Financial-Analyst/financial_advisor/agent.py
+++ Financial-Analyst/financial_advisor/agent.py
@@ -3,10 +3,27 @@ from google.adk.models.lite_llm import LiteLlm
 
 MODEL = LiteLlm("openai/gpt-4o")
 
+def get_weather(city: str):
+    return f"The weather in {city} is 30 degrees."
+
+def conver_units(degrees:int):
+    return f"That is 40 fahrenheit"
+
+geo_agent = Agent(
+    name="GeoAgent",
+    instruction="You help the user with geo related questions",
+    model=MODEL,
+    description="Transfer to this agent when you have a geo related question",
+)
 weather_agent = Agent(
     name="WeatherAgent",
     instruction="You help the user with weather related questions",
     model=MODEL,
+    tools=[
+        get_weather,
+        conver_units,
+        ],
+    sub_agents=[geo_agent],
 )
 
 root_agent = weather_agent 
