104fb94a124863913cd342843d6a4c1cf0aea954
Bitnoori Lee <bitnoori.lee@gmail.com>
2026-02-05 21:53:27 +0100

    add Agent state

--- Financial-Analyst/financial_advisor/agent.py
+++ Financial-Analyst/financial_advisor/agent.py
@@ -1,5 +1,6 @@
 from google.adk.agents import Agent
 from google.adk.tools.agent_tool import AgentTool
+from google.adk.tools import ToolContext
 from google.adk.models.lite_llm import LiteLlm
 from .sub_agents.data_analyst import data_analyst
 from .sub_agents.financial_analyst import financial_analyst
@@ -9,9 +10,27 @@ from .prompt import PROMPT
 MODEL = LiteLlm("openai/gpt-4o")
 
 
-def save_advice_report():
-    pass
+def save_advice_report(tool_context: ToolContext, summary:str):
+    state = tool_context.state
+    data_analyst_result = state.get("data_analyst_result")
+    financial_analyst_result = state.get("financial_analyst_result")
+    news_analyst_result = state.get("news_analyst_result")
+    report = f"""
+    # Executive Summary and Advice:
+    {summary}
 
+    ##Data Analyst Report:
+    {data_analyst_result}
+
+    ##Financial Analyst Report:
+    {financial_analyst_result}
+
+    ##News Analyst Report:
+    {news_analyst_result}
+    """
+    return {
+        "success": True,
+    }
 
 financial_advisor = Agent(
     name="FinancialAdvisor",
